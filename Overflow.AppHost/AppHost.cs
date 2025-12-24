5359d5636c35549efb493b6f10166c530bcdd131
Daniel Laksana <budidarma.laksana@customerservice.nsw.gov.au>
2025-12-24 21:16:23 +1100

    add Question service

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,6 +1,15 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
+//run: dotnet dev-certs https --trust
+#pragma warning disable ASPIRECERTIFICATES001
 var keycloak = builder.AddKeycloak("keycloak", 16001)
-    .WithDataVolume("keycloak-data");
+    .WithDataVolume("keycloak-data")
+    .WithHttpsDeveloperCertificate();
+
+var questionService = builder.AddProject<QuestionService>("question-svc")
+    .WithReference(keycloak)
+    .WaitForStart(keycloak);    
 
 builder.Build().Run();
